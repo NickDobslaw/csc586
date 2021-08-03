ccb5d084b4977fe7caf8d3871a86a889f8d7f32d
NickDobslaw <70283395+NickDobslaw@users.noreply.github.com>
2021-08-03 09:57:43 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,14 +1,11 @@
 import geni.portal as portal
-import geni.rspec.pg as pg
 import geni.rspec.igext as IG
+import geni.rspec.pg as pg
 
 pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 tourDescription = \
-"""
-This profile provides a two node set for Assignmetn two of CSC586
-"""
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
@@ -17,13 +14,13 @@ link = request.LAN("lan")
 
 for i in range(2):
   if i == 0:
-    node = request.XenVM("webserver") #create webserver
+    node = request.XenVM("webserver")
   else:
-    node = request.XenVM("observer")  #create observer
+    node = request.XenVM("observer") 
   if i == 0:
-    node.routable_control_ip = "true" #public ip for webserver
+    node.routable_control_ip = "true"
   else:
-    node.routable_control_ip = "false" #private ip for obsever
+    node.routable_control_ip = "false"
   
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   iface = node.addInterface("if" + str(i))
@@ -35,6 +32,5 @@ for i in range(2):
     node.addService(pg.Execute(shell="sh", command="bash /local/repository/silly.sh"))
   else:
     continue
-  
-# Print the RSpec to the enclosing page.
+ 
 pc.printRequestRSpec(request)
