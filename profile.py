e18ba52266f750cccddabd03dd0cc88e328cb5bb
NickDobslaw <70283395+NickDobslaw@users.noreply.github.com>
2021-08-04 18:20:54 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,16 +1,27 @@
 import geni.portal as portal
 import geni.rspec.pg as pg
+import geni.rspec.igext as IG
 
 pc = portal.Context()
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.XenVM("node")
+
+tourDescription = \
+"""
+This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
+"""
+
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+
+node = request.XenVM("head")
+node.cores = 4
+node.ram = 8192
+node.routable_control_ip = "true" 
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node.routable_control_ip = "true"
-
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="bash /local/repository/silly.sh"))
-
-# Print the RSpec to the enclosing page.
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+  
 pc.printRequestRSpec(request)
